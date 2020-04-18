e8efb5137d63e83fcab7c1469bc6aa133f6ea07b
Itziar Tuya Rivas <Itziartuya@gmail.com>
2020-04-18 15:03:27 +0200

    itial commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<!-- Habitissimo Challenges by Itziar Tuya Rivas -->
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+    <!-- Favicon -->
+    <link href="https://es.habcdn.com/static/root/favicon-32x32.png" rel="icon" type="image/x-icon" sizes="32x32" />
+
+    <title>Habitissimo - Reformas y Servicios para el Hogar</title>
+  </head>
+  <body>
+
+
+<!--Main Layout-->
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Acceder</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span>
+						<a target="_blank" rel="nofollow noopener" data-trackevent="RedesSociales.Facebook.Siguenos" title="Facebook" href="https://www.facebook.com/habitissimo/" class="facebook">
+							<i class="fab fa-facebook-square"></i>
+						</a>
+					</span>
+					<span>
+						<a target="_blank" rel="nofollow noopener" data-trackevent="RedesSociales.Twitter.Siguenos" title="Twitter" href="https://twitter.com/habitissimo" class="twitter">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+					</span>
+					<span>
+						<a target="_blank" rel="nofollow noopener" data-trackevent="RedesSociales.Pinterest.Siguenos" title="Pinterest" href="https://www.pinterest.com/habitissimo/">
+							<i class="fab fa-pinterest-square"></i>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Recordarme
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Entrar" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					¿Aun no tienes cuenta?<a href="#">Regístrate</a>
+				</div>
+				<div class="d-flex justify-content-center forgot_psw">
+					<a href="#">¿Olvidaste tu contraseña?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Main Layout-->
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+
+  <style type="text/css">
+  	/*.nav-item { padding: 50px; }
+  	.navbar-brand { padding-left: 50px; } */
+  </style>
+</html>
+
